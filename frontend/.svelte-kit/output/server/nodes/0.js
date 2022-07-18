59868a11be5e40f6cd2e9fbdb4ef23dabc730397
Lev Stambler <levstamb@gmail.com>
2022-07-17 20:42:43 -0400

    rm svelte kit

--- frontend/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/fallbacks/layout.svelte.js';
-
-export { module };
-export const entry = 'layout.svelte-b82c1f92.js';
-export const js = ["layout.svelte-b82c1f92.js","chunks/index-e461e465.js"];
-export const css = [];
